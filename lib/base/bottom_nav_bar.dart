13c93778334c4194528b64435f842f19a41b4d44
GodwinAkpah <gakpah96@gmail.com>
2024-08-22 10:05:07 +0100

    second commit

---
+++ lib/base/bottom_nav_bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    // return const Placeholder( child: Text("hello"),);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Ticket"),
+      ),
+      body: const Center(
+        child: Text('Ticket info'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+        ],
+      ),
+    );
+  }
+}
